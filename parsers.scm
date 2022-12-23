e3626e4e4f14012bfe35672034412390d9e03ca7
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-22 20:43:17 -0800

    Reformat functions, make explicit

--- parsers.scm
+++ parsers.scm
@@ -2,70 +2,85 @@
 
 ;; === base ===
 
-(define (item lst)
-  (if (null? lst)
-      (list)
-      (list (car lst) (cdr lst))))
+(define item
+  (lambda (lst)
+    (if (null? lst)
+        (list)
+        (list (car lst) (cdr lst)))))
 
 ;; === monad ===
 
-(define (zero) (lambda xs (list)))
+(define zero
+  (lambda ()
+    (lambda xs (list))))
 
-(define (return x)
-  (lambda (input)
-    (list x input)))
+(define return
+  (lambda (x)
+    (lambda (input)
+      (list x input))))
 
-(define (bind p f)
-  (lambda (input)
-    (let ([lst (p input)])
-      (if (null? lst)
-          (list)
-          ((f (car lst)) (cadr lst))))))
+(define bind
+  (lambda (p f)
+    (lambda (input)
+      (let ([lst (p input)])
+        (if (null? lst)
+            (list)
+            ((f (car lst)) (cadr lst)))))))
 
 ;; === functor ===
 
-(define (map-f f p)
-  (bind p (lambda (x) (return (f x)))))
+(define map-f
+  (lambda (f p)
+    (bind p (lambda (x) (return (f x))))))
 
 ;; === applicative ===
 
-(define (apply-p pf px)
-  (bind pf (lambda (f)
-             (map-f f px))))
+(define apply-p
+  (lambda (pf px)
+    (bind pf (lambda (f) (map-f f px)))))
 
 ;; apply-p defined only by bind
 ;;
-;; (define (apply-p pf px)
-;;   (bind pf (lambda (f)
-;;              (bind px (lambda (x)
-;;                         (return (f x)))))))
+;; (define apply-p
+;;   (lambda (pf px)
+;;     (bind pf (lambda (f)
+;;                (bind px (lambda (x)
+;;                          (return (f x))))))))
 
 ;; === alternatives ===
 
-(define (or-else p q)
-  (lambda (input)
-    (let ([lst (p input)])
-      (if (null? lst)
-          (q input)
-          lst))))
+(define or-else
+  (lambda (p q)
+    (lambda (input)
+      (let ([lst (p input)])
+        (if (null? lst)
+            (q input)
+            lst)))))
 
-(define (and-then p q)
-  (bind p (lambda (pv)
-            (bind q (lambda (qv)
-                      (return (list pv qv)))))))
+(define and-then
+  (lambda (p q)
+    (bind p (lambda (pv)
+              (bind q (lambda (qv)
+                        (return (list pv qv))))))))
 
 ;; === derived primitives ===
 
-(define (satisfy predicate)
-  (bind item (lambda (x)
-               (if (predicate x)
-                   (return x)
-                   (zero)))))
+(define satisfy
+  (lambda (predicate)
+    (bind item (lambda (x)
+                 (if (predicate x)
+                     (return x)
+                     (zero))))))
 
-(define (parse-char x) (satisfy (lambda (y) (char=? x y))))
+(define parse-char
+  (lambda (x)
+    (satisfy (lambda (y) (char=? x y)))))
 
-(define parse-digit (satisfy char-numeric?))
+(define parse-digit 
+  (satisfy char-numeric?))
 
-(define parse-alpha (satisfy char-alphabetic?))
+(define parse-alpha 
+  (satisfy char-alphabetic?))
 
-(define parse-space (satisfy char-whitespace?))
+(define parse-space 
+  (satisfy char-whitespace?))
