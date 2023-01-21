b0b1e8c2e2288c51764f66b8c474fa0fd6080342
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-01-21 00:51:17 -0800

    Add functions defined with "do" syntax

--- macros.scm
+++ macros.scm
@@ -1,11 +1,97 @@
 (import (rnrs))
 
-;; Highly-experimental macro for the Haskell "do" syntax
+;; The Haskell "do" syntax
 (define-syntax do
   (lambda (stx)
     (syntax-case stx (<-)
-      [(_ pn)
-       (syntax pn)]
-      [(_ (x <- px) pn ...)
+      [(_ expr)
+       (syntax expr)]
+      [(_ (x <- px) expr ...)
        (syntax (bind px (lambda (x) 
-                          (do pn ...))))])))
+                          (do expr ...))))])))
+
+(define empty? null?)
+
+;; === base ===
+
+(define item
+  (lambda (x)
+    (if (empty? x)
+        x
+        (list (car x) (cdr x)))))
+
+;; === monad ====
+
+;; Also named "unit".
+(define return
+  (lambda (x)
+    (lambda (input)
+      (list x input))))
+
+;; Also named ">>=".
+;; Integrates the sequencing of parsers with the processing of their results.
+(define bind
+  (lambda (px f)
+    (lambda (input)
+      (let ([x (px input)])
+        (if (empty? x)
+            x
+            ((f (car x)) (cadr x)))))))
+
+;; Also named "empty".
+(define zero
+  (lambda ()
+    (lambda input '())))
+
+;; === functor ===
+
+(define map-f
+  (lambda (f px)
+    (do (x <- px)
+        (return (f x)))))
+
+;; === applicative ===
+
+(define apply-p
+  (lambda (pf px)
+    (do (f <- pf)
+        (x <- px)
+        (return (f x)))))
+
+;; (define apply-p
+;;   (lambda (px py)
+;;     (bind px (lambda (x)
+;;                (bind py (lambda (y)
+;;                           (return (x y))))))))
+
+;; === satisfy ===
+
+(define satisfy
+  (lambda (predicate)
+    (do (x <- item)
+        (if (predicate x)
+            (return x)
+            (zero)))))
+
+;; === choices ===
+
+(define or-else
+  (lambda (px py)
+    (lambda (input)
+      (let ([x (px input)])
+        (if (empty? x)
+            (py input)
+            x)))))
+
+(define and-then
+  (lambda (px py)
+    (do (x <- px)
+        (y <- py)
+        (return (cons x y)))))
+
+(define many
+  (lambda (px)
+    (or-else (do (x  <- px)
+                 (xs <- (many px))
+                 (return (cons x xs)))
+             (return '()))))
