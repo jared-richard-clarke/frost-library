f298dd5ed897fc85b44bf2ce7ebd354b84ea67d5
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-01-24 15:24:03 -0800

    Add utils library

---
+++ utils.scm
@@ -0,0 +1,26 @@
+(library (utils)
+         (export empty?
+                 compose
+                 assert-test)
+         (import (scheme))
+         
+         (define empty? null?)
+
+         (define (compose . functions)
+           (lambda (arg)
+             (fold-right (lambda (function value)
+                           (function value))
+                         arg
+                         functions)))
+
+         (define-syntax assert-test
+           (lambda (stx)
+             (syntax-case stx ()
+               [(_ compare expression value)
+                (syntax (let ([computed-expr expression]) ;; <- prevents redundant computation
+                          (unless (compare computed-expr value)
+                            (printf "Test: ~a\nExpect: ~a, Got: ~a\n"
+                                    (quote expression) ;; <---- returns expression prior to evaluation
+                                    value
+                                    computed-expr))))])))
+         )
