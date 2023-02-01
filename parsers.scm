d72bdd602bcced6de139b24b8c31ac1ce6324534
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-01-31 21:16:44 -0800

    Rename pattern variables

--- parsers.scm
+++ parsers.scm
@@ -17,8 +17,8 @@
     (syntax-case stx (<-)
       [(_ expression)
        (syntax expression)]
-      [(_ (x <- px) expression ...)
-       (syntax (bind px (lambda (x) 
+      [(_ (x <- mx) expression ...)
+       (syntax (bind mx (lambda (x) 
                           (monad-do expression ...))))])))
 
 ;; === base ===
