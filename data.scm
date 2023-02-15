efc4918f39ca2abe9b4c08a58583951c9bdca53e
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-14 20:54:40 -0800

    Make data immutable

--- data.scm
+++ data.scm
@@ -7,21 +7,19 @@
 	  EMPTY-OK
 	  EMPTY-ERROR)
   (import (rnrs))
-
+	 
   (define-enumeration Consumer-Enum (CONSUMED EMPTY OK ERROR) make-consumer)
 
   (define CONSUMED-OK    (make-consumer CONSUMED OK))
-  (define CONSUMED-ERROR (make-consumer CONSUMED ERROR))
+  (define CONSUMED-ERROR (make-consumer CONSUMED ERROR)) 
 
   (define EMPTY-OK    (make-consumer EMPTY OK))
   (define EMPTY-ERROR (make-consumer EMPTY ERROR))
 
-  (define-record-type Position
-    (fields (mutable line)
-	    (mutable column)))
+  (define-record-type Position (fields line column))
 
-  (define-record-type Parser-Consumer
-    (fields (mutable consumer) ;; Consumed Ok | Consumed Error | Empty Ok | Empty Error
-            (mutable input)))  ;; (list char)
-            (mutable output)   ;; Any
-  )
+  (define-record-type (Parser-Consumer consume consumer?)
+    (fields type     ;; Consumed Ok | Consumed Error | Empty Ok | Empty Error
+	    input    ;; (list char)
+	    output)) ;; Any
+)
