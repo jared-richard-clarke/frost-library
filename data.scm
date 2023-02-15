8f2742d14efbc433eb4b67f123964d0ec1188a0d
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-15 00:08:43 -0800

    Change enumerations to constants

--- data.scm
+++ data.scm
@@ -1,25 +1,25 @@
 ;; === UNDER HEAVY CONSTRUCTION ===
 
 (library (data)
-  (export Parser-Consumer
-	  CONSUMED-OK
-	  CONSUMED-ERROR
-	  EMPTY-OK
-	  EMPTY-ERROR)
+  (export context
+	  CONSUMED
+	  EMPTY
+	  OK
+	  ERROR
+	  UNKNOWN)
   (import (rnrs))
 	 
-  (define-enumeration Consumer-Enum (CONSUMED EMPTY OK ERROR) make-consumer)
+  (define CONSUMED 'Consumed)
+  (define EMPTY 'Empty)
+  (define OK 'Ok)
+  (define ERROR 'Error)
+  (define UNKNOWN 'UNKNOWN)
 
-  (define CONSUMED-OK    (make-consumer CONSUMED OK))
-  (define CONSUMED-ERROR (make-consumer CONSUMED ERROR)) 
+  (define-record-type position (fields line column))
 
-  (define EMPTY-OK    (make-consumer EMPTY OK))
-  (define EMPTY-ERROR (make-consumer EMPTY ERROR))
-
-  (define-record-type Position (fields line column))
-
-  (define-record-type (Parser-Consumer consume consumer?)
-    (fields type     ;; Consumed Ok | Consumed Error | Empty Ok | Empty Error
-	    input    ;; (list char)
-	    output)) ;; Any
+  (define-record-type context
+    (fields consumed  ;; Consumed | Empty
+	    reply     ;; Ok | Error | Unknown
+	    output    ;; Any
+	    input))   ;; (list char)
 )
