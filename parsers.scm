7562fd3c939c54a3dfe69d10ea39c107217cd7b3
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-16 17:17:52 -0800

    Revise categories for "punctuation"

--- parsers.scm
+++ parsers.scm
@@ -48,7 +48,7 @@
          (define punctuation
            (satisfy (lambda (x)
                       (let ([category    (char-general-category x)]
-                            [categories '(ps pe pi pf pd pc po)])
+                            [categories '(Ps Pe Pi Pf Pd Pc Po)])
                         (symbol-in? category categories)))))
 
          ;; Finds all punctuation as defined by ASCII. Subsumed by Unicode.
