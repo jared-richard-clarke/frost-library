887c3f9bd4f119d5f927bf6e781da83da2637311
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-05-18 14:19:15 -0700

    Add tests

--- test-monads.scm
+++ test-monads.scm
@@ -21,12 +21,9 @@
 ;; The laws tested in this file are a generally agreed upon set of rules
 ;; that monads, alternatives, and functors should follow in order to have
 ;; predictable, common sense behaviour.
-;;
-;; Side Note: Cannot compare functions directly. Must compare their outputs.
-;; In a purely functional language, there are only inputs and outputs.
 
 ;; test data
-;; ...
+(define test-data "abc")
 
 ;; === Monad Laws ===
 ;;
@@ -36,11 +33,19 @@
 
 ;; return a >>= f = f a
 ;;  unit a * λb.n = n[a/b]
-;; ...
+(let ([rhs (parse (bind (return #\a) character) test-data)]
+      [lhs (parse (character #\a) test-data)])
+  (let ([ctx-x (context->list rhs)]
+        [ctx-y (context->list lhs)])
+    (assert equal? ctx-x ctx-y)))
 
 ;;  p >>= return = p
 ;; m * λa.unit a = m
-;; ...
+(let ([rhs (parse (bind (character #\a) return) test-data)]
+      [lhs (parse (character #\a) test-data)])
+  (let ([ctx-x (context->list rhs)]
+        [ctx-y (context->list lhs)])
+    (assert equal? ctx-x ctx-y)))
 
 ;;   (p >>= f) >>= g = p >>= (\a -> f a >>= g)
 ;; (m * λa.n) * λb.o = m * (λa.n * λb.o)
@@ -52,11 +57,26 @@
 ;;        px <|> zero = px <-
 ;; px <|> (py <|> pz) = (px <|> py) <|> pz <-- Associativity
 
-;; zero <|> px = px <|> zero <- Identity
-;; ...
+;; zero <|> px = px <- Identity
+(let ([rhs (parse (or-else zero (character #\a)) test-data)]
+      [lhs (parse (character #\a) test-data)])
+  (let ([ctx-x (context->list rhs)]
+        [ctx-y (context->list lhs)])
+    (assert equal? ctx-x ctx-y)))
+
+;; px <|> zero = px <- Identity
+(let ([rhs (parse (or-else (character #\a) zero) test-data)]
+      [lhs (parse (character #\a) test-data)])
+  (let ([ctx-x (context->list rhs)]
+        [ctx-y (context->list lhs)])
+    (assert equal? ctx-x ctx-y)))
 
 ;; px <|> (py <|> pz) = (px <|> py) <|> pz <- Associativity
-;; ...
+(let ([rhs (parse (or-else (character #\b) (or-else (character #\a) (character #\c))) test-data)]
+      [lhs (parse (or-else (or-else (character #\b) (character #\a)) (character #\c)) test-data)])
+  (let ([ctx-x (context->list rhs)]
+        [ctx-y (context->list lhs)])
+    (assert equal? ctx-x ctx-y)))
 
 ;; === Functor Laws ===
 ;;
@@ -64,7 +84,19 @@
 ;; fmap (g . f) = fmap g . fmap f <-- Composition
 
 ;; fmap id = id <- Identity
-;; ...
+(let ([rhs (parse (fmap identity (character #\a)) test-data)]
+      [lhs (parse (character #\a) test-data)])
+  (let ([ctx-x (context->list rhs)]
+        [ctx-y (context->list lhs)])
+    (assert equal? ctx-x ctx-y)))
 
 ;; fmap (g . f) = fmap g . fmap f <- Composition
-;; ...
+(let ([curry-map (lambda (f)
+                   (lambda (px) (fmap f px)))])
+  (let ([ff (curry-map string-length)]
+        [fg (curry-map string)])
+    (let ([rhs (parse (fmap (compose string-length string) (character #\a)) test-data)]
+          [lhs (parse ((compose ff fg) (character #\a)) test-data)])
+      (let ([ctx-x (context->list rhs)]
+            [ctx-y (context->list lhs)])
+        (assert equal? ctx-x ctx-y)))))
