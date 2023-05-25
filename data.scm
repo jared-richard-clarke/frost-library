51b89ccb7d752940fce4d8a0081f03ed2a284a13
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-05-24 21:06:07 -0700

    Revise documentation

--- data.scm
+++ data.scm
@@ -5,11 +5,13 @@
                  state)
          (import (rnrs))
 
+         ;; Tracks input as it is consumed by parser.
          (define-record-type state
            (fields input    ;; (list character)
                    line     ;; number
                    column)) ;; number
 
+         ;; Tracks parser consumption, state, and output.
          (define-record-type context
            (fields reply    ;;  Consumed Ok | Consumed Error | Empty Ok | Empty Error
                    state    ;; (list character) number number
