b7406d31b3657594727bb1617f9ec82fed1ecb20
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-06-05 22:42:21 -0700

    Update tests to match code base

--- test-monads.scm
+++ test-monads.scm
@@ -5,19 +5,6 @@
         (parsers)
         (utils))
 
-;; Converts `context` record to list for deep structural comparison with `equal?`
-(define context->list
-  (lambda (ctx)
-    (if (not (and (record? ctx) (eq? (record-type-name (record-rtd ctx)) 'context)))
-        (error ctx "argument must be a record of type 'context")
-        (let* ([reply  (context-reply  ctx)]
-               [state  (context-state  ctx)]
-               [input  (state-input    state)]
-               [line   (state-line     state)]
-               [column (state-column   state)]
-               [output (context-output ctx)])
-          (list reply (list input line column) output)))))
-
 ;; === Unit Tests: Monads, Alternatives, Functors ===
 ;;
 ;; The laws tested in this file are a generally agreed upon set of rules
@@ -35,27 +22,21 @@
 
 ;; return a >>= f = f a
 ;;  unit a * λb.n = n[a/b]
-(let ([rhs (parse (bind (return #\a) character) test-data)]
-      [lhs (parse (character #\a) test-data)])
-  (let ([ctx-x (context->list rhs)]
-        [ctx-y (context->list lhs)])
-    (assert equal? ctx-x ctx-y)))
+(assert equal?
+        (parse-string (bind (return #\a) character) test-data)
+        (parse-string (character #\a) test-data))
 
 ;;  p >>= return = p
 ;; m * λa.unit a = m
-(let ([rhs (parse (bind (character #\a) return) test-data)]
-      [lhs (parse (character #\a) test-data)])
-  (let ([ctx-x (context->list rhs)]
-        [ctx-y (context->list lhs)])
-    (assert equal? ctx-x ctx-y)))
+(assert equal?
+        (parse-string (bind (character #\a) return) test-data)
+        (parse-string (character #\a) test-data))
 
 ;;   (p >>= f) >>= g = p >>= (\a -> f a >>= g)
 ;; (m * λa.n) * λb.o = m * (λa.n * λb.o)
-(let ([rhs (parse (bind (bind (return #\a) character) return) test-data)]
-      [lhs (parse (bind (return #\a) (lambda (x) (bind (character x) return))) test-data)])
-  (let ([ctx-x (context->list rhs)]
-        [ctx-y (context->list lhs)])
-    (assert equal? ctx-x ctx-y)))
+(assert equal?
+        (parse-string (bind (bind (return #\a) character) return) test-data)
+        (parse-string (bind (return #\a) (lambda (x) (bind (character x) return))) test-data))
 
 ;; === Alternative and/or MonadPlus Laws ===
 ;;
@@ -64,25 +45,19 @@
 ;; px <|> (py <|> pz) = (px <|> py) <|> pz <-- Associativity
 
 ;; zero <|> px = px <- Identity
-(let ([rhs (parse (or-else zero (character #\a)) test-data)]
-      [lhs (parse (character #\a) test-data)])
-  (let ([ctx-x (context->list rhs)]
-        [ctx-y (context->list lhs)])
-    (assert equal? ctx-x ctx-y)))
+(assert equal?
+        (parse-string (or-else zero (character #\a)) test-data)
+        (parse-string (character #\a) test-data))
 
 ;; px <|> zero = px <- Identity
-(let ([rhs (parse (or-else (character #\a) zero) test-data)]
-      [lhs (parse (character #\a) test-data)])
-  (let ([ctx-x (context->list rhs)]
-        [ctx-y (context->list lhs)])
-    (assert equal? ctx-x ctx-y)))
+(assert equal?
+        (parse-string (or-else (character #\a) zero) test-data)
+        (parse-string (character #\a) test-data))
 
 ;; px <|> (py <|> pz) = (px <|> py) <|> pz <- Associativity
-(let ([rhs (parse (or-else (character #\b) (or-else (character #\a) (character #\c))) test-data)]
-      [lhs (parse (or-else (or-else (character #\b) (character #\a)) (character #\c)) test-data)])
-  (let ([ctx-x (context->list rhs)]
-        [ctx-y (context->list lhs)])
-    (assert equal? ctx-x ctx-y)))
+(assert equal?
+        (parse-string (or-else (character #\b) (or-else (character #\a) (character #\c))) test-data)
+        (parse-string (or-else (or-else (character #\b) (character #\a)) (character #\c)) test-data))
 
 ;; === Functor Laws ===
 ;;
@@ -90,19 +65,15 @@
 ;; fmap (g . f) = fmap g . fmap f <-- Composition
 
 ;; fmap id = id <- Identity
-(let ([rhs (parse (fmap identity (character #\a)) test-data)]
-      [lhs (parse (character #\a) test-data)])
-  (let ([ctx-x (context->list rhs)]
-        [ctx-y (context->list lhs)])
-    (assert equal? ctx-x ctx-y)))
+(assert equal?
+        (parse-string (fmap identity (character #\a)) test-data)
+        (parse-string (character #\a) test-data))
 
 ;; fmap (g . f) = fmap g . fmap f <- Composition
 (let* ([curry-map (lambda (f)
                     (lambda (px) (fmap f px)))]
        [ff (curry-map string-length)]
        [fg (curry-map string)])
-  (let ([rhs (parse (fmap (compose string-length string) (character #\a)) test-data)]
-        [lhs (parse ((compose ff fg) (character #\a)) test-data)])
-    (let ([ctx-x (context->list rhs)]
-          [ctx-y (context->list lhs)])
-      (assert equal? ctx-x ctx-y))))
+  (assert equal?
+          (parse-string (fmap (compose string-length string) (character #\a)) test-data)
+          (parse-string ((compose ff fg) (character #\a)) test-data)))
