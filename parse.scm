83f7a5d6b78c8918e64aa8a1c3d2a76f1c18fb83
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-11-10 13:22:08 -0800

    Parameterize state

--- parse.scm
+++ parse.scm
@@ -3,6 +3,7 @@
          (import (rnrs base)
                  (rnrs io simple)
                  (frost combinators)
+                 (frost data)
                  (frost parsers))
          
          ;; Applies a parser to a string and outputs an arbitrary value dictated by the parser.
@@ -11,7 +12,7 @@
          (define parse-string
            (lambda (parser text)
              (let-values ([(reply state want output)
-                           (parser (make-state (string->list text) 1 0))])
+                           (parser (make-state (string->list text) 1 0 state-update-char))])
                (begin (write reply)
                       (newline)
                       (write state)
