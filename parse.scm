040cda521f4e7a2f7f1af7cc880b9a1487c7942a
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-11-12 15:42:47 -0800

    Update parse.scm

--- parse.scm
+++ parse.scm
@@ -6,12 +6,10 @@
                  (frost data)
                  (frost parsers))
          
-         ;; Applies a parser to a string and outputs a tagged result. Lifts string into monadic context
-         ;; before applying parser.
-         ;; (parse (text "abc") "abc") -> result:struct(OK, "abc")
-         ;; (parse (text "abc") "xyz") -> result:struct(ERROR, "line: 1, column: 1. Expected: abc, Got: x")
-         ;; Side Note: prints "reply", "state", "want", and "output" to the standard output for debugging.
-         ;; This is temporary.
+         ;; Applies a parser to a string and outputs a tagged result.
+         ;; Lifts string into monadic context before applying parser.
+         ;; (parse (text "abc") "abc") -> struct:result{OK, "abc"}
+         ;; (parse (text "abc") "xyz") -> struct:result{ERROR, "line: 1, column: 1. Expected: abc, Got: x"}
          (define parse
            (lambda (parser text)
              (let-values ([(reply state want output)
@@ -21,16 +19,8 @@
                                           [line   1]
                                           [column 1])
                                      (make-state input length offset line column)))])
-               (begin (write reply)
-                      (newline)
-                      (write state)
-                      (newline)
-                      (write want)
-                      (newline)
-                      (write output)
-                      (newline)
-                      (if (eq? (cdr reply) ERROR)
-                          (make-result ERROR (format-error state want))
-                          (make-result OK    output))))))
+               (if (eq? (cdr reply) ERROR)
+                   (make-result ERROR (format-error state want))
+                   (make-result OK    output)))))
          
          )
