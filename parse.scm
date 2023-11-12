98400e9953ed7ba2cd317a51a2a293be91982772
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-11-12 15:16:48 -0800

    Update parse.scm

--- parse.scm
+++ parse.scm
@@ -1,18 +1,26 @@
 (library (frost parse)
-         (export parse-string)
+         (export parse)
          (import (rnrs base)
                  (rnrs io simple)
                  (frost combinators)
                  (frost data)
                  (frost parsers))
          
-         ;; Applies a parser to a string and outputs an arbitrary value dictated by the parser.
+         ;; Applies a parser to a string and outputs a tagged result. Lifts string into monadic context
+         ;; before applying parser.
+         ;; (parse (text "abc") "abc") -> result:struct(OK, "abc")
+         ;; (parse (text "abc") "xyz") -> result:struct(ERROR, "line: 1, column: 1. Expected: abc, Got: x")
          ;; Side Note: prints "reply", "state", "want", and "output" to the standard output for debugging.
          ;; This is temporary.
-         (define parse-string
+         (define parse
            (lambda (parser text)
              (let-values ([(reply state want output)
-                           (parser (make-state (string->list text) 1 0))])
+                           (parser (let* ([input  (string->vector text)]
+                                          [length (vector-length input)]
+                                          [offset 0]
+                                          [line   1]
+                                          [column 1])
+                                     (make-state input length offset line column)))])
                (begin (write reply)
                       (newline)
                       (write state)
