bd56e2b8bda1fb01551da05fd0e18c0dee51906d
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-11-12 15:30:32 -0800

    Reformat

--- test-monads.scm
+++ test-monads.scm
@@ -5,9 +5,9 @@
         (frost parse)
         (frost utils))
 
-;; R6RS does not support structural equality for record types. "parse-compare" converts 
+;; R6RS does not support structural equality for record types. "compare" converts 
 ;; "result" record type to list for deep, structural comparison via "equal?".
-(define parse-compare
+(define compare
   (lambda (parse parser text)
     (let* ([result (parse parser text)]
            [flag   (result-flag result)]
@@ -32,20 +32,20 @@
 ;; return a >>= f = f a
 ;;  unit a * λb.n = n[a/b]
 (assert equal?
-        (parse-compare parse-string (bind (return #\a) character) test-data)
-        (parse-compare parse-string (character #\a)               test-data))
+        (compare parse (bind (return #\a) character) test-data)
+        (compare parse (character #\a)               test-data))
 
 ;;  p >>= return = p
 ;; m * λa.unit a = m
 (assert equal?
-        (parse-compare parse-string (bind (character #\a) return) test-data)
-        (parse-compare parse-string (character #\a)               test-data))
+        (compare parse (bind (character #\a) return) test-data)
+        (compare parse (character #\a)               test-data))
 
 ;;   (p >>= f) >>= g = p >>= (\a -> f a >>= g)
 ;; (m * λa.n) * λb.o = m * (λa.n * λb.o)
 (assert equal?
-        (parse-compare parse-string (bind (bind (return #\a) character) return)                  test-data)
-        (parse-compare parse-string (bind (return #\a) (lambda (x) (bind (character x) return))) test-data))
+        (compare parse (bind (bind (return #\a) character) return)                  test-data)
+        (compare parse (bind (return #\a) (lambda (x) (bind (character x) return))) test-data))
 
 ;; === Alternative and/or MonadPlus Laws ===
 ;;
@@ -55,34 +55,34 @@
 
 ;; zero <|> px = px <- Identity
 (assert equal?
-        (parse-compare parse-string (or-else zero (character #\a)) test-data)
-        (parse-compare parse-string (character #\a)                test-data))
+        (compare parse (or-else zero (character #\a)) test-data)
+        (compare parse (character #\a)                test-data))
 
 ;; px <|> zero = px <- Identity
 (assert equal?
-        (parse-compare parse-string (or-else (character #\a) zero) test-data)
-        (parse-compare parse-string (character #\a)                test-data))
+        (compare parse (or-else (character #\a) zero) test-data)
+        (compare parse (character #\a)                test-data))
 
 ;; px <|> (py <|> pz) = (px <|> py) <|> pz <- Associativity
 (assert equal?
-        (parse-compare parse-string (or-else (character #\b) (or-else (character #\a) (character #\c))) test-data)
-        (parse-compare parse-string (or-else (or-else (character #\b) (character #\a)) (character #\c)) test-data))
+        (compare parse (or-else (character #\b) (or-else (character #\a) (character #\c))) test-data)
+        (compare parse (or-else (or-else (character #\b) (character #\a)) (character #\c)) test-data))
 
 ;; === Functor Laws ===
 ;;
 ;; fmap id = id <-------------------- Identity
-;; fmap (g . f) = fmap g . fmap f <-- Composition
+;; fmap (g ∘ f) = fmap g ∘ fmap f <-- Composition
 
 ;; fmap id = id <- Identity
 (assert equal?
-        (parse-compare parse-string (fmap identity (character #\a)) test-data)
-        (parse-compare parse-string (character #\a)                 test-data))
+        (compare parse (fmap identity (character #\a)) test-data)
+        (compare parse (character #\a)                 test-data))
 
-;; fmap (g . f) = fmap g . fmap f <- Composition
+;; fmap (g ∘ f) = fmap g ∘ fmap f <- Composition
 (let* ([curry-map (lambda (f)
                     (lambda (px) (fmap f px)))]
        [ff (curry-map string-length)]
        [fg (curry-map string)])
   (assert equal?
-          (parse-compare parse-string (fmap (compose string-length string) (character #\a)) test-data)
-          (parse-compare parse-string ((compose ff fg) (character #\a))                     test-data)))
+          (compare parse (fmap (compose string-length string) (character #\a)) test-data)
+          (compare parse ((compose ff fg) (character #\a))                     test-data)))
