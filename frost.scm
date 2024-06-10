f1e537e856b52981e8b8d2faa48f493446ebebb4
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-06-10 14:01:56 -0700

    Consolidate interface into a single library

---
+++ frost.scm
@@ -0,0 +1,69 @@
+(library (frost)
+  ;; === combinators: parser building blocks ===
+  (export monad-do
+          <-
+          return
+          bind
+          empty
+          fail
+          fmap
+          satisfy
+          try
+          label
+          choice
+          optional
+          either
+          replace
+          left
+          right
+          between
+          sequence
+          many
+          many-1
+          skip-many
+          sep-by
+          sep-by-1
+          end-by
+          end-by-1
+          chain-left
+          chain-left-1
+          chain-right
+          chain-right-1
+  ;; === parsers: predefined parsers ===
+          one-of
+          none-of
+          singleton
+          any
+          digit
+          denary-digit
+          binary-digit
+          digits
+          whole
+          integer
+          real
+          rational
+          letter
+          letters
+          upper-case
+          lower-case
+          alpha-num
+          space
+          spaces
+          skip-spaces
+          linefeed
+          crlf
+          tab
+          punctuation
+          punctuation-ascii
+          trim-left
+          trim-right
+          trim
+          chunk
+          text
+  ;; === parse: applies parser to input string ===
+          parse)
+  (import (frost combinators)
+          (frost parsers)
+          (frost parse)
+          (frost data)
+          (frost utils)))
