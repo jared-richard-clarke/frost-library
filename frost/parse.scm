db893033b3aac786a8eac16726362926c4655972
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-06-13 17:29:53 -0700

    Create parse.scm

---
+++ frost/parse.scm
@@ -0,0 +1,25 @@
+(library (frost parse)
+         (export parse)
+         (import (rnrs base)
+                 (frost combinators)
+                 (frost data)
+                 (frost parsers))
+         
+         ;; Applies a parser to a string and outputs a tagged result.
+         ;; Lifts string into monadic context before applying parser.
+         ;; (parse (text "abc") "abc") -> struct:result{OK, "abc"}
+         ;; (parse (text "abc") "xyz") -> struct:result{ERROR, "line: 1, column: 1. Expected: abc, Got: x"}
+         (define parse
+           (lambda (parser text)
+             (let-values ([(reply state want output)
+                           (parser (let* ([input  (string->vector text)]
+                                          [length (vector-length input)]
+                                          [offset 0]
+                                          [line   1]
+                                          [column 1])
+                                     (make-state input length offset line column)))])
+               (if (eq? (cdr reply) ERROR)
+                   (make-result ERROR (format-error state want))
+                   (make-result OK    output)))))
+         
+         )
