7d0adc6c1a1d633053df513c0df1ec633cf4fdc9
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-08-29 17:44:09 -0700

    Check input before parsing

--- frost/matcher.scm
+++ frost/matcher.scm
@@ -1,9 +1,11 @@
 (library (frost matcher)
-         (export parse)
+         (export match)
          (import (rnrs base)
                  (frost combinators)
                  (frost data)
                  (frost parsers))
+
+         (define INPUT-ERROR "Input must be of type string.")
          
          ;; Applies a parser to a string and outputs a tagged result.
          ;; Lifts string into monadic context before applying parser.
@@ -11,15 +13,17 @@
          ;; (match (text "abc") "xyz") -> struct:result{ERROR, "line: 1, column: 1. Expected: abc, Got: x"}
          (define match
            (lambda (parser text)
-             (let-values ([(reply state want output)
-                           (parser (let* ([input  (string->vector text)]
-                                          [length (vector-length input)]
-                                          [offset 0]
-                                          [line   1]
-                                          [column 1])
-                                     (make-state input length offset line column)))])
-               (if (eq? (cdr reply) ERROR)
-                   (make-result ERROR (format-error state want))
-                   (make-result OK    output)))))
+             (if (not (string? text))
+                 (make-result ERROR INPUT-ERROR)
+                 (let-values ([(reply state want output)
+                               (parser (let* ([input  (string->vector text)]
+                                              [length (vector-length input)]
+                                              [offset 0]
+                                              [line   1]
+                                              [column 1])
+                                         (make-state input length offset line column)))])
+                   (if (eq? (cdr reply) ERROR)
+                       (make-result ERROR (format-error state want))
+                       (make-result OK    output))))))
          
          )
